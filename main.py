231c4020963faba4ed02644ac98e8245be603dbe
Susceptance <46062232+Suscept@users.noreply.github.com>
2022-03-27 23:23:10 -0700

    Created very basic UI

---
+++ main.py
@@ -0,0 +1,8 @@
+from readWrite import f2dmtxEncode
+from PIL import Image
+
+filePath = input('Enter path to file:\n')
+
+outputPath = input('Enter output path. Leave blank to output to the same path as the input file.\n')
+
+f2dmtxEncode(filePath).save('dmtx.png')
