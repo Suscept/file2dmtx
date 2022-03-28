344cd1cd9f47967b8c8c36e1c4509e30663e7c42
Susceptance <46062232+Suscept@users.noreply.github.com>
2022-03-28 16:08:33 -0700

    Added success message

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ filePath = input('Enter path to file:\n')
 outputPath = input('Enter output path. Leave blank to output to the same path as the input file.\n')
 
 f2dmtxEncode(filePath).save('dmtx.png')
+print('File saved as dmtx.png')
