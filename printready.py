ee7ffefcbb2bf86909ab44e2a6dd3a5f2f96e264
Susceptance <46062232+Suscept@users.noreply.github.com>
2022-04-08 23:55:28 -0700

    Added ready-to-print image generator
    Script to take a data matrix and size and label it for printing

---
+++ printready.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw, ImageFont
+
+#width = 816
+#height = 1056
+
+width = 750
+height = 971
+
+def MakePrintReady(image: Image, label1: str, label2: str):
+    """
+    Returns the provided data matrix labeled and sized for printing on A4 paper.
+
+    `label1` goes on the top left
+
+    `label2` goes on the top right
+    """
+    image = image.resize((width, width))
+
+    im = Image.new('RGB', (width, height), 'white')
+    im.paste(image, (0, height - width))
+
+    d = ImageDraw.Draw(im)
+    d.text((0, 0), label1, fill=('black'), font=ImageFont.truetype('arial', 24))
+    d.text((width, 0), label2, fill=('black'), font=ImageFont.truetype('arial', 24), anchor="ra")
+
+    return im
