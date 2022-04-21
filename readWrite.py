21c3f1408d9ed47da31f4476ab59b15ded5d861e
Susceptance <46062232+Suscept@users.noreply.github.com>
2022-04-20 22:34:37 -0700

    Added data length to encode tuple

--- readWrite.py
+++ readWrite.py
@@ -75,7 +75,7 @@ def f2dmtxEncode(data: bytes, filename: str):
     for i, item in enumerate(partitions):
         encoded = encode(item, 'base256', )
         img = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
-        matrixImg = (filename, img, i, len(partitions))
+        matrixImg = (filename, img, i, len(partitions), len(item))
         images.append(matrixImg)
 
     return images
